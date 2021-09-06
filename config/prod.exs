5d645c39fe331382b9142d624e5b8577bd768d99
Joe Williams <jowi@pop-os.localdomain>
2021-09-06 10:04:35 -0400

    distillery config

--- config/prod.exs
+++ config/prod.exs
@@ -17,19 +17,27 @@ config :logger, level: :info
 # To get SSL working, you will need to add the `https` key
 # to the previous section and set your `:url` port to 443:
 #
-config(:daily_twang, DailyTwangWeb.Endpoint, ...,
-  url: [host: "${APP_NAME}.gigalixir.com", port: 443],
+config :daily_twang, DailyTwangWeb.Endpoint,
+  url: [host: "${APP_NAME}.gigalixirapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: "${SECRET_KEY_BASE}",
-  https: [
-    ...,
-    port: {:system, "PORT"},
-    cipher_suite: :strong,
-    keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-    certfile: System.get_env("SOME_APP_SSL_CERT_PATH"),
-    force_ssl: [hsts: true]
-  ]
-)
+  # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
+  http: [port: {:system, "PORT"}],
+  # Needed for Phoenix 1.3. Doesn't hurt for other versions
+  load_from_system_env: true,
+  # Without this line, your app will not start the web server!
+  server: true,
+  # To bust cache during hot upgrades
+  version: Mix.Project.config()[:version]
+
+config :daily_twang, DailyTwang.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: "${DATABASE_URL}",
+  # Works around a bug in older versions of ecto. Doesn't hurt for other versions.
+  database: "",
+  ssl: true,
+  # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
+  pool_size: 2
 
 # The `cipher_suite` is set to `:strong` to support only the
 # latest and more secure SSL ciphers. This means old browsers
